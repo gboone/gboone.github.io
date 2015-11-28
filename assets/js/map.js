f581899b99611563bcfc2115864add82925cdb44
gboone <boone.greg@gmail.com>
2015-11-28 09:34:02 -0600

    Adds mapping filter and Stamen tiles over SSL

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,12 @@
-// replace "toner" here with "terrain" or "watercolor"
-var layer = new L.StamenTileLayer("terrain");
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
 var map = new L.Map("map", {
 });
 latlng = new L.LatLng(map._container.dataset.lat, map._container.dataset.lng)
 map.setView(latlng, 12)
-map.addLayer(layer);
+map.addLayer(Stamen_TonerLite);
