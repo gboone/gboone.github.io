8df8eea187a0feb84a79e9388943881298045887
gboone <boone.greg@gmail.com>
2015-12-05 14:22:48 -0600

    A journey to the UP + mapping capability

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,32 +7,56 @@ var Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z
 
 var map = new L.Map("map", { scrollWheelZoom: false });
 if ( map._container.dataset.lat && map._container.dataset.lng ) {
-	var latlng = setLatLng(map._container.dataset)
-	map.setView(latlng, 12)
-} else {
-	var mapData = load_data()
-	var bounds = []
-	var markers = _.each(mapData, function(item) {
-		var latlng = setLatLng(item);
-		bounds.push(latlng);
-		L.marker(latlng, {
-			clickable: true,
-			title: item['name'],
-			alt: "A marker indicating the location of " + item['name']
-		}).addTo(map)
-			.bindPopup(item['name']);
+	var item = map._container.dataset
+	setMarker(item)
+	var latlng = [setLatLng(item)]
+	if ( ! document.getElementById('locations') ) {
+		map.fitBounds(latlng)
+		map.setZoom(12)
+	}
+}
+if (document.getElementById('locations')) {
+	var locations = loadData('locations')
+	var mapData = loadData('map-data')
+	var reduced = []
+	_.each(mapData, function(item) {
+		reduced.push(locations[item])
 	})
-	bounds = new L.latLngBounds(bounds)
-	map.fitBounds(bounds)
+	setMarkers(reduced)
+	map.fitBounds(setBounds(reduced))
 }
 map.addLayer(Toner);
 
-function load_data() {
-	return JSON.parse(document.getElementById('map-data').innerHTML);
+function loadData(id) {
+	return JSON.parse(document.getElementById(id).innerHTML);
 }
 
 function setLatLng(dataset) {
 	var lat = dataset.lat
 	var lng = dataset.lng
 	return new L.LatLng(lat, lng)
+}
+
+function setMarker(item) {
+	var latlng = setLatLng(item);
+	L.marker(latlng, {
+		clickable: true,
+		title: item['name'],
+		alt: "A marker indicating the location of " + item['name']
+	}).addTo(map)
+		.bindPopup(item['name']);
+}
+function setMarkers(mapData) {
+	var markers = _.each(mapData, function(item) {
+		setMarker(item)
+	})
+}
+
+function setBounds(mapData) {
+	var bounds = []
+	_.each(mapData, function(item) {
+			var latlng = setLatLng(item)
+			bounds.push(latlng)
+	})
+	return bounds
 }
